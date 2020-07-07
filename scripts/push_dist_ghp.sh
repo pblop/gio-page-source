6293e0db7e7951e452e938948a2eac6f21234699
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2020-07-07 14:26:35 +0200

    Actually commit and push

--- scripts/push_dist_ghp.sh
+++ scripts/push_dist_ghp.sh
@@ -4,12 +4,15 @@ export CLONE_LOCATION="$HOME/github-page"
 export GIT_ASKPASS="$GITHUB_WORKSPACE/scripts/git_askpass_helper.sh"
 
 cd $GITHUB_WORKSPACE
-
 export COMMIT_HASH=$(git rev-parse HEAD)
+
+cd $CLONE_LOCATION
 
 # Configure git alias
 git config user.email "action@github.com"
 git config user.name "GitHub Action"
+
+git add .
 
 git commit -m "Automated build of $COMMIT_HASH"
 
