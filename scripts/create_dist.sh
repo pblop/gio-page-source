f10a57a315b1eb7e5215675f4beaeb923147da02
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2020-07-07 15:23:34 +0200

    Don't delete .git

--- scripts/create_dist.sh
+++ scripts/create_dist.sh
@@ -17,8 +17,8 @@ git clone "https://pabloworker@github.com/PabloPerezRodriguez/pabloperezrodrigue
 echo $(ls $CLONE_LOCATION)
 
 # Delete every file in the github.io page repo but keep the CNAME file.
-echo $(find "$CLONE_LOCATION" -mindepth 1 -not -name 'CNAME')
-find "$CLONE_LOCATION" -mindepth 1 -not -name 'CNAME' -delete
+echo $(find "$CLONE_LOCATION" -mindepth 1 -not -name 'CNAME' -not -name '.git')
+find "$CLONE_LOCATION" -mindepth 1 -not -name 'CNAME' -not -name '.git' -delete
 
 
 ###### Actually build ######
