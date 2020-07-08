62c641bc4afa753e9fd7708a3d555ce18a4446f7
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2020-07-08 02:02:13 +0200

    last test for today

--- scripts/create_dist.sh
+++ scripts/create_dist.sh
@@ -26,8 +26,4 @@ cd "$GITHUB_WORKSPACE"
 $YARN_PATH install
 $YARN_PATH build
 
-echo "gw: $GITHUB_WORKSPACE"
-echo "gwd: $GITHUB_WORKSPACE/dist"
-echo "cl: $CLONE_LOCATION/"
-
-cp -vr $GITHUB_WORKSPACE/dist/ $CLONE_LOCATION/
+cp -vr "$GITHUB_WORKSPACE/dist/." "$CLONE_LOCATION/"
